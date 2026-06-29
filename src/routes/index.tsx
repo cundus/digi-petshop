b9b6004b880d8c57a3cdb6801c5b05fa505879e8
Sundus Ardian <83276960+cundus@users.noreply.github.com>
2026-06-29 21:22:45 +0700

    Updated index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -19,7 +19,7 @@ import { Logo } from "@/components/Logo";
 import { SITE, waLink } from "@/lib/site";
 
 import heroPets from "@/assets/hero-pets.jpg";
-import aboutShop from "@/assets/about-us.jpg.asset.json";
+import aboutShop from "@/assets/about-us.jpg";
 
 export const Route = createFileRoute("/")({
   head: () => ({
